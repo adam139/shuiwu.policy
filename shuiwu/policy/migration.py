59d0de71c26aaf5813220a3679d20047aede3d87
adam139 <yuejun.tang@gmail.com>
2016-10-23 11:37:41 +0800

    add update step for create subtree

--- shuiwu/policy/migration.py
+++ shuiwu/policy/migration.py
@@ -1,138 +1,71 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFCore.utils import getToolByName
-from plone.dexterity.utils import createContentInContainer
-from my315ok.socialorgnization.content.orgnization import IOrgnization,IOrgnization_annual_survey
+from shuiwu.baoshui.content.nashuiren import Inashuiren
 
-from my315ok.socialorgnization.content.orgnizationfolder import IOrgnizationFolder
-from my315ok.socialorgnization.content.page import IPage
-from plone.app.contenttypes.behaviors.richtext import IRichText
-
-from plone.i18n.normalizer.interfaces import INormalizer
-from zope.component import getUtility
-from Acquisition import aq_parent
-from plone.app.textfield.value import RichTextValue
-
-def migrate2Document(context):
-    "migrate my315ok.socialorgnization.page to plone.app.contenttypes.Document"
+subids = [('zichanfuzaibiao1','yuedujilu',u'资产负债表',12),
+               ('lirunbiao1','yuedujilu',u'利润表',12),
+               ('xianjinliuliangbiao1','yuedujilu',u'现金流量表',12),
+               ('chengjianjiaoyudifangfujia','yuedujilu',u'城建、教育、地方教育附加申报表',12),
+               ('gerensuodeshui1','yuedujilu',u'个人所得税扣缴表',12),
+               ('zhifugongzimingxi1','yuedujilu',u'支付工资明细',12),
+               ('yinhuashuianyue1','yuedujilu',u'印花税申报表（按月）',12),
+               ('canbaojinshenbaobiao','yuedujilu',u'残保金申报表',12),
+               ('gonghuijingfei1','yuedujilu',u'工会经费申报表',12),
+               ('shuilijijin1','yuedujilu',u'水利基金申报表（月报）',12),
+               ('shebaofei1','yuedujilu',u'社保费申报表',12),
+               ('fangchanshui1','yuedujilu',u'房产税申报表（租金收入）',12),
+               ('tudizengzhishui1','yuedujilu',u'土地增值税申报表（按月）',12),
+               ('anyueqita1','yuedujilu',u'其他1',12),
+               ('anyueqita2','yuedujilu',u'其他2',12),
+               ('qiyesuodeshuialeiblei','jidujilu',u'企业所得税预缴表（A类、B类）',4),
+               ('fangchanshuifangchanyuanzhi1','jidujilu',u'房产税申报表（房产原值）',4),
+               ('chengzhentudishiyongshui1','jidujilu',u'城镇土地使用税申报表',4), 
+               ('anjiqita1','jidujilu',u'按季其他1',4),
+               ('anjiqita2','jidujilu',u'按季其他2',4),
+               ('yinhuashuizijinzhangbo1','ancijilu',u'印花税申报表（资金帐薄）',12),
+               ('ziyuanshui1','ancijilu',u'资源税申报表',12),
+               ('gengdizhanyongshui1','ancijilu',u'耕地占用税申报表',12),
+               ('anciqita','ancijilu',u'其他',12)                                                                                                           
+               ]
+def createChildTree(context):
+    "for nashuiren create sub-child-tree"
+    #search all nashuiren objects that have not sub object
     pc = getToolByName(context, "portal_catalog")
-#    wf = getToolByName(context, 'portal_workflow')
-    query = {"object_provides":IPage.__identifier__}
+    query = {"object_provides":Inashuiren.__identifier__}
     bns = pc(query)
-    for bn in bns:
-        ob = bn.getObject()
-        id = "%s2" % (bn.id)
-        title = bn.Title
-        description = bn.Description
-        text = ob.text.output
-        pt = aq_parent(ob)
-        odate = bn.created
 
-        try:
-            item =createContentInContainer(pt,"Document",
-                                           checkConstraints=False,
-                                           id=id,
-                                           title=title,
-                                           description=description)
-            
-            IRichText(item).text = RichTextValue(text,'text/html','text/html')
-            item.creation_date = odate
-            item.setModificationDate(odate)
-            item.reindex(idxs=['created', 'modified'])
-#             pt.manage_delObjects([bn.id])            
-        except:
-            continue        
- 
- 
-def deletesocialorgnizationpage(context):
-    
-    pc = getToolByName(context, "portal_catalog")
-#    wf = getToolByName(context, 'portal_workflow')
-    query = {"object_provides":IPage.__identifier__}
-    bns = pc(query)    
-    
-
-
-#     print >> buf, "Found %d items to be purged" % len(bns)
-
-#     count = 0
-    for b in bns:
-#         count += 1
-        obj = b.getObject()
-#         print >> buf, "Deleting:" + obj.absolute_url() + " " + str(obj.created())
-        obj.aq_parent.manage_delObjects([obj.getId()])
+    emptysubtreelists = filter(pathsearchFilter,bns)
+    finishlist = map(mapf,emptysubtreelists)
         
-#     return buf.getvalue()        
-
-
-def set_defaultview(context):
-    "将旧的社会组织年检数据设置默认视图view"
+def getTargetobj(context,objid):
+    "get target nashuiren object that has been created sub tree by object id"
     pc = getToolByName(context, "portal_catalog")
-#    wf = getToolByName(context, 'portal_workflow')
-    query = {"object_provides":IOrgnization_annual_survey.__identifier__}
+    query = {"object_provides":Inashuiren.__identifier__,'id':objid}
     bns = pc(query)
-    for bn in bns:
-        if bn.review_state  == "published":  # after modify workflow and add directly publish transition.
-            ob = bn.getObject()
-            ob.setLayout("view")
-
-#            wf.doActionFor(ob, 'publish', comment='迁移数据，将原始年检直接标记为发布状态。' )
-
-def publish_survey(context):
-    "将旧的社会组织年检数据直接发布为published状态"
-    pc = getToolByName(context, "portal_catalog")
-    wf = getToolByName(context, 'portal_workflow')
-    query = {"object_provides":IOrgnization_annual_survey.__identifier__}
-    bns = pc(query)
-    for bn in bns:
-        if bn.review_state  == "draft":  # after modify workflow and add directly publish transition.
-            ob = bn.getObject()
-
-            wf.doActionFor(ob, 'publish', comment='迁移数据，将原始年检直接标记为发布状态。' )
-
-
-def publish_organization(context):
-    "将旧的社会组织数据直接发布为published状态"
-    pc = getToolByName(context, "portal_catalog")
-    wf = getToolByName(context, 'portal_workflow')
-    query = {"object_provides":IOrgnization.__identifier__}
-    bns = pc(query)
-    for bn in bns:
-        if bn.review_state  == "private":
-            ob = bn.getObject()
-           
-            wf.doActionFor(ob, 'publish', comment='old org init as published status' )
+    return bns[0].getObject()
         
-    
-    
-def init_governmentdepartment(context):
-    "将归属湘潭市的社会组织提取出来，为每个对象创建一个上级监管单位"
-    
+def mapf(brain):
+    "copy & paster sub tree for the brain"
+
+    target = brain.getObject()
+    source = getTargetobj(target,"43062519700816052X01")       
+    for subid,subtype,title,num in subids:
+        subobj = source[subid]
+        api.content.copy(source=subobj, target=target)
+
+
+def pathsearchFilter(brain):
+    "search the specify brain, path of the brain,if the path has sub-object,return True" 
+    context = brain.getObject()
     pc = getToolByName(context, "portal_catalog")
-    query = {"object_provides":IOrgnizationFolder.__identifier__}
+    query = {"path":"/".join(context.getPhysicalPath())}
     bns = pc(query)
-    folder = bns[0].getObject()
-    query = {"object_provides":IOrgnization.__identifier__,"orgnization_belondtoArea":"xiangtanshi"}
-    bns = pc(query)
-    # create government department data
-    title2id = getUtility(INormalizer,'zh')
-    ts = [i.orgnization_supervisor for i in bns]
-    bsset = set(ts)
-    for title in bsset:
-        if title == "" or title == None:continue        
-        if not isinstance(title, unicode):
-            title = unicode(title, 'utf-8')                    
-        # call title to id utility
-        id = title2id.normalize(title)
-        try:
-            item =createContentInContainer(folder,
-                                           "my315ok.socialorgnization.governmentorgnization",
-                                           checkConstraints=False,
-                                           id=id,
-                                           title=title)
-            item.reindex()
-#            setattr(item,'title',title)
-        except:
-            pass
+    if len(bns) <= 1:
+        return True
+    else:
+        return False       
+
 
         
         
