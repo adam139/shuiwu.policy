51adf25c47f4d2f3a3fb63453609d2168eef3646
adam139 <yuejun.tang@gmail.com>
2016-12-17 11:21:03 +0800

    fix replace description step

--- shuiwu/policy/migration.py
+++ shuiwu/policy/migration.py
@@ -119,8 +119,8 @@ def resetDescription(context):
     pc = getToolByName(context, "portal_catalog")
     query = {"object_provides":Inashuiren.__identifier__}
     bns = pc(query)
-#     import pdb
-#     pdb.set_trace()
+    import pdb
+    pdb.set_trace()
     bns = filter(zipFilter,bns)
     if len(bns) > 5:
         bns = bns[:4]    
